7ebd29482821635f9a6d5a13fb3df6f50691a7a8
Adrien Abbey <abbey.12@wright.edu>
2022-02-27 13:33:37 -0500

    Added partial filename fields

--- Suit.java
+++ Suit.java
@@ -9,16 +9,18 @@
 // https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
 
 public enum Suit {
-    HEARTS("Hearts"),
-    DIAMONDS("Diamonds"),
-    SPADES("Spades"),
-    CLUBS("Clubs");
+    HEARTS("Hearts", "_of_hearts"),
+    DIAMONDS("Diamonds", "_of_diamonds"),
+    SPADES("Spades", "_of_spades"),
+    CLUBS("Clubs", "_of_clubs");
 
     /* Enum Fields */
     public final String label;
+    public final String filename; // contains the relevant partial file name for the given suit
 
     /* Enum Constructors */
-    private Suit(String label) {
+    private Suit(String label, String filename) {
         this.label = label;
+        this.filename = filename;
     }
 }
