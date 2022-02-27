394684b8b4e8cfbc854de5f30cf4279eaa29450c
Adrien Abbey <abbey.12@wright.edu>
2022-02-27 13:33:26 -0500

    Added partial filenames fields

--- Rank.java
+++ Rank.java
@@ -9,27 +9,29 @@
 // https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
 
 public enum Rank {
-    ACE("Ace", 1),
-    TWO("Two", 2),
-    THREE("Three", 3),
-    FOUR("Four", 4),
-    FIVE("Five", 5),
-    SIX("Six", 6),
-    SEVEN("Seven", 7),
-    EIGHT("Eight", 8),
-    NINE("Nine", 9),
-    TEN("Ten", 10),
-    JACK("Jack", 11),
-    QUEEN("Queen", 12),
-    KING("King", 13);
+    ACE("Ace", 1, "ace"),
+    TWO("Two", 2, "2"),
+    THREE("Three", 3, "3"),
+    FOUR("Four", 4, "4"),
+    FIVE("Five", 5, "5"),
+    SIX("Six", 6, "6"),
+    SEVEN("Seven", 7, "7"),
+    EIGHT("Eight", 8, "8"),
+    NINE("Nine", 9, "9"),
+    TEN("Ten", 10, "10"),
+    JACK("Jack", 11, "jack"),
+    QUEEN("Queen", 12, "queen"),
+    KING("King", 13, "king");
 
     /* Enum Fields */
     public final String label;
     public final int value;
+    public final String filename;
 
     /* Enum Constructor */
-    private Rank(String label, int value) {
+    private Rank(String label, int value, String filename) {
         this.label = label;
         this.value = value;
+        this.filename = filename;
     }
 }
