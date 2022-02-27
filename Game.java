1efa883eac2b3ce61512be4bc481b62f45f80d8b
Adrien Abbey <abbey.12@wright.edu>
2022-02-26 19:09:36 -0500

    Add / clean up class fields, renamed variables, minor polish

--- Game.java
+++ Game.java
@@ -9,19 +9,20 @@ import java.util.Random;
 public class Game {
 
     /* Fields */
-    private int pot; // How many tokens are in the pot
-    private ArrayList<Player> playerList = new ArrayList<>();
-    private int pTurn; // Tracks which player's turn it is.
     private Random rng = new Random();
-    private Deck gameDeck; // Game constructor will create a the deck with provided properties
+    private ArrayList<Player> playerList = new ArrayList<>();
+    private int pot; // How many tokens are in the pot
+    private int pTurn; // Tracks which player's turn it is.
+    private int rScore; // Track the current round's value
+    private Deck drawDeck;
     private Deck discardDeck;
 
     /* Constructor */
     public Game(String deckType, int deckCount, Player player1, Player player2, Player player3, Player player4) {
-        // Start a new game with the given players:
+        // Start a new game with the given values:
 
-        // Create a new deck to draw from:
-        gameDeck = new Deck(deckType, deckCount);
+        // Create new card decks:
+        drawDeck = new Deck(deckType, deckCount);
         discardDeck = new Deck();
 
         // The game starts with an empty pot:
@@ -43,23 +44,26 @@ public class Game {
         // New round, new player's turn:
         pTurn++;
 
+        // Reset the current score:
+        rScore = 0;
+
         // Return all the cards in each player's hand back to the game deck:
         for (Player p : playerList) {
-            p.getHand().emptyInto(gameDeck);
+            p.getHand().emptyInto(drawDeck);
         }
 
         // Return all the discarded cards to the game deck:
-        discardDeck.emptyInto(gameDeck);
+        discardDeck.emptyInto(drawDeck);
 
         // Reshuffle the deck:
-        gameDeck.shuffle();
+        drawDeck.shuffle();
 
         // Deal a card to each player 3 times:
         for (int i = 0; i < 3; i++) {
             // For each player:
             for (Player p : playerList) {
                 // Deal a card to the player:
-                p.drawCard(gameDeck);
+                p.drawCard(drawDeck);
             }
         }
     }
@@ -69,8 +73,8 @@ public class Game {
         // Return true if successful, false there's no cards left in the deck.
 
         // If there's a card left in the deck to draw:
-        if (gameDeck.size() > 0) {
-            p.drawCard(gameDeck);
+        if (drawDeck.size() > 0) {
+            p.drawCard(drawDeck);
             return true;
         } else {
             return false;
@@ -80,6 +84,11 @@ public class Game {
     public void addToPot() {
         // Add a token to the pot:
         pot++;
+    }
+
+    public int getScore() {
+        // Return the round's current score:
+        return rScore;
     }
 
     public int getPot() {
@@ -96,13 +105,13 @@ public class Game {
     public void printHands() {
         // FIXME: TEST: Print out each player's hands:
         for (Player p : playerList) {
-            System.out.println(p.getHand());
+            System.out.println(p.getName() + "'s hand: " + p.getHand());
         }
     }
 
     public void printDeckSizes() {
         // FIXME: TEST: Print out the deck sizes:
-        System.out.println("Game deck size: " + gameDeck.size());
+        System.out.println("Game deck size: " + drawDeck.size());
         System.out.println("Discard pile size: " + discardDeck.size());
     }
 
