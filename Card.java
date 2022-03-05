78587ff7684347fe9c5f6e52b6a070b5c387a98c
Adrien Abbey <abbey.12@wright.edu>
2022-03-04 21:31:56 -0500

    implemented Comparable method, added getScoreValue method

--- Card.java
+++ Card.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
-public class Card {
+public class Card implements Comparable<Card> {
 
     /* Fields */
 
@@ -45,6 +45,27 @@ public class Card {
         return suit.label;
     }
 
+    public int getScoreValue() {
+        // Returns the score value of the card.
+        // 10s return -10, 9s and 4s return 0, and Kings return 99.
+        // All other cards return their point values.
+
+        // If this card is a king:
+        if (this.rank.value == 13) {
+            // Return 99:
+            return 99;
+        } else if (this.rank.value == 10) {
+            // If a 10, return -10:
+            return -10;
+        } else if (this.rank.value == 9 || this.rank.value == 4) {
+            // If a 9 or a 4, return 0:
+            return 0;
+        } else {
+            // For all other cards, return their rank value:
+            return this.rank.value;
+        }
+    }
+
     public String getImageFilePath() {
         // Return the full path for the given card's image file:
         return ("./PNG-cards-1.3/" + rank.filename + suit.filename + ".png");
@@ -67,6 +88,54 @@ public class Card {
         imageIcon = new ImageIcon(resizedImage);
 
         return imageIcon;
+    }
+
+    @Override
+    public int compareTo(Card other) {
+        // This comparable method override is intended to provide ComputerPlayers with a
+        // way to sort their current hand. Cards are sorted based on the desirability to
+        // discard, where high rank non-special cards are most desirable to discard,
+        // while special cards should be kept as long as possible.
+
+        // Note: for sake of randomness and simplicity, all special cards have the same
+        // compare values.
+
+        // TODO: Verify the order of cards. Consider giving Kings a different value
+        // later.
+
+        // If this card is a special card:
+        if (this.rank.value == 13 || this.rank.value == 10 || this.rank.value == 9 || this.rank.value == 4) {
+            // If the other card is also special:
+            if (other.rank.value == 13 || other.rank.value == 10 || other.rank.value == 9 || other.rank.value == 4) {
+                // Then these cards have the same value:
+                return 0;
+            } else {
+                // Otherwise, the other card is not special, and therefore more desirable to
+                // discard:
+                return 1;
+            }
+        } else {
+            // If this card is not special, but the other card is special:
+            if (other.rank.value == 13 || other.rank.value == 10 || other.rank.value == 9 || other.rank.value == 4) {
+                // Then this non-special card is more desirable to discard than the other
+                // special card:
+                return -1;
+            } else {
+                // Otherwise, both cards are non-special, so compare their rank values:
+                if (this.rank.value > other.rank.value) {
+                    // If this card has a higher rank value than the other card, then this card has
+                    // a higher discard desirability:
+                    return -1;
+                } else if (this.rank.value < other.rank.value) {
+                    // If this card has a lower rank value than the other card, then this card has a
+                    // lower discard desirability:
+                    return 1;
+                } else {
+                    // Otherwise both cards have the same value:
+                    return 0;
+                }
+            }
+        }
     }
 
     @Override
