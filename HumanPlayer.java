8bbff72b72f94884524cc18aabb95eb200f534c0
Adrien Abbey <abbey.12@wright.edu>
2022-03-05 22:30:14 -0500

    First attempt at the player's playTurn method, added playCard, isWaiting methods

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -3,6 +3,8 @@
 
 public class HumanPlayer extends Player {
     /* Fields */
+    boolean waiting; // Tracks whether the game is waiting for input from the player.
+    int playCard; // Tracks what card the player selected.
 
     /* Constructor */
     public HumanPlayer(String name, int tokens) {
@@ -10,8 +12,32 @@ public class HumanPlayer extends Player {
     }
 
     /* Methods */
-    public boolean playTurn() {
-        // FIXME TEST: Return true for testing purposes.
-        return true;
+    public void playTurn() {
+        // FIXME TEST: This needs to work right.
+
+        // Set to waiting for input:
+        waiting = true;
+
+        // So long as the player hasn't played a card
+        while (waiting) {
+            // FIXME: Just wait for input. Is this okay?
+        }
+
+        // Play the selected card:
+        this.playCard(playCard);
+
+        // Update the score:
+        NinetyNine.game.updateScore();
+    }
+
+    public void playCard(int cardIndex) {
+        // Sets the played card index and stops waiting:
+        this.playCard = cardIndex;
+        waiting = false;
+    }
+
+    public boolean isWaiting() {
+        // Return waiting status:
+        return waiting;
     }
 }
