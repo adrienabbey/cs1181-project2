d42cc948515143526819c3ad851d0d8063666074
Adrien Abbey <abbey.12@wright.edu>
2022-03-06 17:35:20 -0500

    track if players are still playing, removed unnecesary abstract methods, added event messages, added isPlaying and isOut methods

--- Player.java
+++ Player.java
@@ -10,6 +10,7 @@ public abstract class Player {
     private String name; // Each player has a name
     private Deck hand; // contains the cards in the player's hand
     private int tokens; // how many tokens the player has
+    private boolean isPlaying; // whether the player is still in the game
 
     /* Constructor */
 
@@ -18,15 +19,16 @@ public abstract class Player {
         hand = new Deck();
         this.tokens = tokens;
         this.name = name;
+        isPlaying = true;
     }
 
     /* Methods */
 
-    public abstract void playTurn();
+    public abstract void playTurn(); // Humans and computers play very differently.
 
-    public abstract boolean isWaiting();
+    // public abstract boolean isWaiting(); // FIXME: I shouldn't need this!
 
-    public abstract void playCard(int playCard);
+    // public abstract void playCard(int playCard); // FIXME: I shouldn't need this!
 
     public void drawCard(Deck deck) {
         // Draws a card from the given deck, moving it to the player's hand:
@@ -35,9 +37,11 @@ public abstract class Player {
         hand.add(card);
     }
 
-    public boolean lost(Game game) {
+    public boolean lostRound(Game game) {
         // The player lost the round. Return true if they can keep playing:
         // TODO: Reveal the player's hand if they lose.
+
+        System.out.println(getName() + " lost the round.");
 
         if (tokens > 0) {
             // If the player has a token, toss it in the pot:
@@ -46,6 +50,7 @@ public abstract class Player {
             return true;
         } else {
             // If the player has no tokens left, they lose:
+            System.out.println(getName() + " is out of the game.");
             return false;
         }
     }
@@ -63,6 +68,16 @@ public abstract class Player {
     public String getName() {
         // Getter: return the player's name:
         return name;
+    }
+
+    public boolean isPlaying() {
+        // Getter: return whether the player is still in the game or not:
+        return isPlaying;
+    }
+
+    public void isOut() {
+        // Setter: the player ran out of tokens and is out of the game:
+        isPlaying = false;
     }
 
     @Override
