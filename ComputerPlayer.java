71c699f755abdabe1dda7e20c73f4e7a4a996640
Adrien Abbey <abbey.12@wright.edu>
2022-03-08 10:11:13 -0500

    Removed unnecessary comments

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -57,20 +57,10 @@ public class ComputerPlayer extends Player {
             System.out.println(this.getName() + " played " + playCard);
         } else {
             // Print out the card played:
-            System.out.println(this.getName() + " played " + this.getHand().get(0)); // FIXME: Why is this throwing an
-                                                                                     // IndexOutOfBoundsException?
+            System.out.println(this.getName() + " played " + this.getHand().get(0));
 
             // No playable card, just play the first card in the hand:
             this.getHand().playCard(0, NinetyNine.game.getDiscardDeck());
         }
     }
-
-    // public boolean isWaiting() {
-    // // The PC is never waiting for input:
-    // return false;
-    // }
-
-    // public void playCard(int playCard) {
-    // // This method should never do anything for a computer player.
-    // }
 }
